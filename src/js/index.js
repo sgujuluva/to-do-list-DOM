28ed19de282dc823f7ac4995d6d5fdb38c0c92e1
sgujuluva <sanghee04@gmail.com>
2022-06-02 14:01:00 +0200

    creating function to add values to li

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-let userInput = document.querySelector("#user-input").value;
+
+
+//select the ul
+let ul = document.querySelector("ul");
+//add event listener to submit
+let submit = document.querySelector("submit").addEventListener("submit",getUserInput);
+
+//function to create li
+
+let getUserInput = (e) => {
+    e.preventDefault();
+    // getting the value from the user and declaring to the variable
+    let userInput = document.querySelector("#user-input").value;
+    if(!userInput == ""){
+        // create li
+        let newLi = document.createElement("li");
+        // text
+        let text = document.createTextNode(userInput);
+        //append to li
+
+    }
+}
